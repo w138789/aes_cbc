1ffd8cf1d6480926b684b462278fd18d3408e34c
w138789 <294496623@qq.com>
2019-11-25 19:01:59 +0800

    11

--- app/common.php
+++ app/common.php
@@ -26,3 +26,49 @@ function send_mail($text)
     $smtp->sendmail($smtpemailto, $smtpusermail, $mailsubject, $mailbody, $mailtype);
 
 }
+
+// 应用公共文件
+function send_mail_qq($text)
+{
+    $smtpserver     = "smtp.163.com";              //SMTP服务器
+    $smtpserverport = 25;                      //SMTP服务器端口
+    $smtpusermail   = "sujianxun123456@163.com";      //SMTP服务器的用户邮箱
+    $smtpuser       = "sujianxun123456@163.com";         //SMTP服务器的用户帐号
+    $smtppass       = "w7026546";                 //SMTP服务器的用户密码
+    $smtpemailto    = "294496623@qq.com";       //发送给谁
+    $mailsubject    = "明天天气";        //邮件主题
+    $mailbody       = $text;      //邮件内容
+    $mailtype       = "TXT";                      //邮件格式（HTML/TXT）,TXT为文本邮件
+    $smtp           = new SMTP($smtpserver, $smtpserverport, true, $smtpuser, $smtppass);
+    $smtp->debug    = false;                     //是否显示发送的调试信息
+    $smtp->sendmail($smtpemailto, $smtpusermail, $mailsubject, $mailbody, $mailtype);
+
+}
+
+/**
+ * curl get 或 pust
+ * @param $url
+ * @param null $data
+ * @return mixed
+ */
+function httpRequest($url, $data = null, $header = false)
+{
+    $curl = curl_init();
+    if ($header == true)
+    {
+        $this_header = ["Content-Type:text/html;charset=utf-8"];
+        curl_setopt($curl, CURLOPT_HTTPHEADER, $this_header);
+    }
+    curl_setopt($curl, CURLOPT_URL, $url);
+    curl_setopt($curl, CURLOPT_SSL_VERIFYPEER, FALSE);
+    curl_setopt($curl, CURLOPT_SSL_VERIFYHOST, FALSE);
+    if (!empty($data))
+    {
+        curl_setopt($curl, CURLOPT_POST, 1);
+        curl_setopt($curl, CURLOPT_POSTFIELDS, $data);
+    }
+    curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
+    $output = curl_exec($curl);
+    curl_close($curl);
+    return $output;
+}
--- app/index/controller/Index.php
+++ app/index/controller/Index.php
@@ -1,10 +1,23 @@
 <?php
+
 namespace app\index\controller;
 
-class Index
+use think\Controller;
+
+class Index extends Controller
 {
     public function index()
     {
-        return '首页';
+        return $this->fetch();
+    }
+
+    public function weather()
+    {
+        $url    = 'https://www.tianqiapi.com/api/?version=v1&cityid=101280101&appid=79153923&appsecret=z4wwVL4o';
+        $result = httpRequest($url);
+        $result = json_decode($result, true);
+        $string = '广州 ' . $result['data'][1]['day'] . ' 气温: 最高 ' . $result['data'][1]['tem1'] . ' 最低 ' . $result['data'][1]['tem2'] . ' ' . $result['data'][1]['index'][3]['title'] . ' 等级 ' . $result['data'][1]['index'][3]['level'] . ' ' . $result['data'][1]['index'][3]['desc'];
+        $result = send_mail_qq($string);
+        return $result;
     }
 }
