ddd98bc186b0412d9139bfe80d7b11e1ab15f9a9
w138789 <294496623@qq.com>
2019-12-10 10:01:08 +0800

    11

--- app/index/controller/Index.php
+++ app/index/controller/Index.php
@@ -11,10 +11,11 @@ class Index extends Controller
         return $this->fetch();
     }
 
-    public function weather()
+    public static function weather()
     {
         $url    = 'https://www.tianqiapi.com/api/?version=v1&cityid=101280101&appid=79153923&appsecret=z4wwVL4o';
         $result = httpRequest($url);
+        var_dump($result);exit;
         $result = json_decode($result, true);
         $string = '广州 ' . $result['data'][1]['day'] . ' 气温: 最高 ' . $result['data'][1]['tem1'] . ' 最低 ' . $result['data'][1]['tem2'] . ' ' . $result['data'][1]['index'][3]['title'] . ' 等级 ' . $result['data'][1]['index'][3]['level'] . ' ' . $result['data'][1]['index'][3]['desc'];
         $result = send_mail_qq($string);
