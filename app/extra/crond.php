789de32ef9df23de33c36498d2c177ff0bb921f4
w138789 <294496623@qq.com>
2020-01-13 11:12:18 +0800

    11

--- app/extra/crond.php
+++ app/extra/crond.php
@@ -10,7 +10,7 @@ $crond_list = array(
     ],
 
     '20:00' => ['app\index\controller\Index::weather'],  //每天某时
-    '10:00' => ['app\index\controller\Index::remind'],  //每天某时
+    //'10:00' => ['app\index\controller\Index::remind'],  //每天某时
 
     //'*:50' => ['app\rearend\controller\Crond::supplierDiscount'],  //每小时订单收货
     //'02:10'      => ['app\rearend\controller\Crond::digitalRightsCondition'],  //每天某时
